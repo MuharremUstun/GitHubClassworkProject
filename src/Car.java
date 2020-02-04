047c751fff96230920f63addbc7f4f512a7ef0b5
MuharremUstun <56238963+MuharremUstun@users.noreply.github.com>
2020-02-03 23:10:43 -0500

    created a new class called Car

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    public static void main(String[] args) {
+        int age;
+        String color;
+        boolean used;
+        double engineVolume;
+        String brand;
+
+
+
+    }
+}
